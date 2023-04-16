f23b5261285e712ad3d5e8bb06dfb7cbf85ab595
Praise Dare <praisedare27@gmail.com>
2023-04-16 13:49:07 +0100

    Solve Exercise 11

--- src/11-record.problem.ts
+++ src/11-record.problem.ts
@@ -1,7 +1,9 @@
 import { expect, it } from "vitest";
 
 const createCache = () => {
-  const cache = {};
+  const cache: {
+    [x: string]: any,
+  } = {};
 
   const add = (id: string, value: string) => {
     cache[id] = value;
